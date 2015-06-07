893deef07ad048f31ef1c77da74fdd5b4749484f
Justin Masse <me@justinmasse.com>
2015-06-06 22:20:05 -0400

    explore thumnbnail changes

--- content-explore.php
+++ content-explore.php
@@ -10,11 +10,22 @@ if(has_post_thumbnail( $postId ) ):
 	$imgSrc = etheme_get_image(get_post_thumbnail_id($postId));
 endif;
 ?>
-<a href="<?php the_permalink(); ?>">
-	<div class="explore-item somepadding col-sm-12 col-xs-12 col-md-<?php echo $col_width; ?>">
+<!-- <a href="<?php the_permalink(); ?>">
+	<div class="explore-item somepadding>
 		<div class="explore-item-container">
 			<div class="bg"  style="background: url(<?php echo $imgSrc; ?>);"></div>
 			<h3><?php the_title(); ?></h3>
 		</div>
 	</div>
-</a>
+</a> -->
+
+<figure class="effect-goliath explore-item  col-sm-12 col-xs-12 col-md-<?php echo $col_width; ?>">
+	<div class="bg">
+		<div class="bg-image"  style="background: url(<?php echo $imgSrc; ?>);"></div>
+	</div>
+	<figcaption>
+		<h2><?php the_title(); ?></h2>
+		<p><?php the_excerpt(); ?></p>
+		<a href="<?php the_permalink(); ?>">View more</a>
+	</figcaption>			
+</figure>
